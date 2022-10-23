9ada795f1d99f58bad3d32321db0fd6500f785b3
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-10-23 15:24:49 +0100

    new states to display content from API

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,30 +3,61 @@ import axios from "axios";
 
 const Users = () => {
   const [getUsers, setGetUsers] = useState([]);
-  const BASE_URL = "https://randomuser.me/api/";
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState("name");
+  const [value, setValue] = useState("random person");
 
-  const fectchUsers = async () => {
+  const BASE_URL = "https://randomuser.me/api/";
+  const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+
+  const fectchUser = async () => {
     try {
       const { data } = await axios.get(BASE_URL);
-      setGetUsers(data.results);
-    } catch (error) {
-      console.log(error);
+      const person = data.results[0];
+      const { phone, email } = person;
+      const {
+        login: { password },
+      } = person;
+      const { first, last } = person.name;
+      const { large: image } = person.picture;
+      const {
+        dob: { age },
+      } = person;
+      const {
+        street: { number, name },
+      } = person.location;
+      const newPerson = {
+        image,
+        phone,
+        email,
+        password,
+        age,
+        name: `${first} ${last}`,
+        street: `${number} ${name}`,
+      };
+      setPerson(newPerson);
+      setTitle("name");
+      setValue(newPerson.name);
+      setLoading(false);
+      // setGetUsers(data.results);
+    } catch (e) {
+      console.log(e);
     }
   };
   useEffect(() => {
-    fectchUsers();
+    fectchUser();
   }, []);
 
   return (
     <section>
-      <div>
-        {getUsers.map((item) => {
-            return(
-                <div>
-                    <h1>{item.name}</h1>
-                </div>
-            )
-        })}
+      <div className="container">
+        <img
+          src={(person && person.image) || defaultImage}
+          alt="random user"
+          className="user-image"
+        />
+        <p className="user-title">My {title} is <strong>{value}</strong></p>
       </div>
     </section>
   );
